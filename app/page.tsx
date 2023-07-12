b5eeb40062abba7b57c504cadd92c331d7bb29d8
Abhinav Malhotra <113239170+abhinavmalhotra01@users.noreply.github.com>
2023-07-13 05:27:52 +0530

    :/

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import EmptyState from "@/app/components/EmptyState";
 import getListings, { IListingsParams } from "@/app/actions/getListings";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import ClientOnly from "./components/ClientOnly";
-
+export const dynamic = "force-dynamic";
 interface HomeProps {
   searchParams: IListingsParams;
 }
